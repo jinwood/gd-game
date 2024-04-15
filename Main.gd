732e3c87c790838c0a345205a3f057933f0ca3b3
jinwood <jinw@protonmail.com>
2024-04-15 21:33:47 +0100

    Add UI and music

--- Main.gd
+++ Main.gd
@@ -5,7 +5,7 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_game()
+	pass	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,15 +16,22 @@ func _process(delta):
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 	
 func new_game():
-	print_debug("hello")
+	$Music.play()
 	score = 0
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 	$Player.start($StartPosition.position)
+	get_tree().call_group("mobs", "queue_free")
 	$StartTimer.start()
 
 func _on_score_timer_timeout():
 	score +=1
+	$HUD.update_score(score)
 	
 func _on_start_timer_timeout():
 	$MobTimer.start()
